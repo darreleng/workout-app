98381b4d63f24c4c9c77e2913242dfb68a21ec29
Darrel <128795218+darreleng@users.noreply.github.com>
2026-01-26 10:19:14 +0800

    Removed better-auth username login plugin

--- backend/src/auth.ts
+++ backend/src/auth.ts
@@ -15,8 +15,5 @@ export const auth = betterAuth({
         clientId: process.env.GITHUB_CLIENT_ID as string, 
         clientSecret: process.env.GITHUB_CLIENT_SECRET as string, 
         }, 
-    },
-    plugins: [
-        username()
-    ],
+    }
 });
