9b5b31f924f51350ffd5f16fd48cde6a1b9c4b4e
Darrel <128795218+darreleng@users.noreply.github.com>
2026-02-04 22:12:08 +0800

    Refactor set code to work properly with frontend

--- backend/src/controllers/setController.ts
+++ backend/src/controllers/setController.ts
@@ -1,15 +1,27 @@
-import { SetSchema } from "@shared/schemas";
 import * as SetModel from "src/models/setModel";
 import { Request, Response } from "express";
 
+// export async function createSet(req: Request, res: Response) {
+//     try {
+//         const result = SetSchema.safeParse(req.body);
+//         if (!result.success) return res.status(400).json({ errors: result.error.issues[0].message });
+//         const userId = req.user!.id;
+//         const newExercise = await SetModel.createSet(userId, {...result.data});
+//         if (!newExercise) return res.status(403).json({ message: "You don't own this exercise."});
+//         res.status(201).json(newExercise);
+//     } catch (error) {
+//         console.error(error);
+//         res.status(500).json( { message: "Internal server error" });
+//     }
+// }
+
 export async function createSet(req: Request, res: Response) {
     try {
-        const result = SetSchema.safeParse(req.body);
-        if (!result.success) return res.status(400).json({ errors: result.error.issues[0].message });
+        const { exerciseId } = req.body;
         const userId = req.user!.id;
-        const newExercise = await SetModel.createSet(userId, {...result.data});
-        if (!newExercise) return res.status(403).json({ message: "You don't own this exercise."});
-        res.status(201).json(newExercise);
+        const newSet = await SetModel.createSet(userId, exerciseId);
+        if (!newSet) return res.status(403).json({ message: "You don't own this exercise."});
+        res.status(201).json(newSet);
     } catch (error) {
         console.error(error);
         res.status(500).json( { message: "Internal server error" });
--- backend/src/models/setModel.ts
+++ backend/src/models/setModel.ts
@@ -1,5 +1,5 @@
 import { pool as db } from "src/db/db";
-import type { SetType } from "@shared/schemas";
+// import type { SetType } from "@shared/schemas";
 
 export async function getSets(userId: string, workoutId: string) {
     const query = 'SELECT s.* FROM sets s JOIN exercises e ON s.exercise_id = e.id JOIN workouts w ON e.workout_id = w.id WHERE w.id = $2 AND w.user_id = $1';
@@ -7,9 +7,15 @@ export async function getSets(userId: string, workoutId: string) {
     return rows;
 }
 
-export async function createSet(userId: string, setData: SetType) {
-    const { exercise_id, set, weight_kg, reps, rest_seconds }  = setData;
-    const query = 'INSERT INTO sets (exercise_id, set_number, weight_kg, reps, rest_seconds) SELECT $2, $3, $4, $5, $6 WHERE EXISTS (SELECT 1 FROM exercises where id = $2 AND user_id = $1) RETURNING *'
-    const { rows } = await db.query(query, [userId, exercise_id, set, weight_kg, reps, rest_seconds]);
-    return rows;
+// export async function createSet(userId: string, setData: SetType) {
+//     const { exercise_id, set, weight_kg, reps, rest_seconds }  = setData;
+//     const query = 'INSERT INTO sets (exercise_id, set_number, weight_kg, reps, rest_seconds) SELECT $2, $3, $4, $5, $6 WHERE EXISTS (SELECT 1 FROM exercises where id = $2 AND user_id = $1) RETURNING *'
+//     const { rows } = await db.query(query, [userId, exercise_id, set, weight_kg, reps, rest_seconds]);
+//     return rows;
+// }
+
+export async function createSet(userId: string, exerciseId: string) {
+    const query = 'INSERT INTO sets (exerciseId) SELECT $2 WHERE EXISTS (SELECT 1 FROM exercises where id = $2 AND user_id = $1) RETURNING *'
+    const { rows } = await db.query(query, [userId, exerciseId]);
+    return rows[0];
 }
