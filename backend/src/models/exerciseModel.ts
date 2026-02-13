39abf3f08e2b76715656163a6f80856c81c62d32
Darrel <128795218+darreleng@users.noreply.github.com>
2026-02-13 22:02:51 +0800

    New exercise now always comes with 1 set

--- backend/src/models/exerciseModel.ts
+++ backend/src/models/exerciseModel.ts
@@ -1,9 +1,23 @@
 import { pool as db } from "src/db/db";
 
 export async function createExercise(userId: string, workoutId: string, exerciseName: string) {
-    const query = 'INSERT INTO exercises (workout_id, name) SELECT $2, $3 WHERE EXISTS (SELECT 1 FROM workouts where id = $2 AND user_id = $1) RETURNING *'
-    const { rows } = await db.query(query, [userId, workoutId, exerciseName]);
-    return rows[0];
+    const client = await db.connect();
+    try {
+        await client.query('BEGIN');
+        
+        const exRes = await db.query('INSERT INTO exercises (workout_id, name) SELECT $2, $3 WHERE EXISTS (SELECT 1 FROM workouts where id = $2 AND user_id = $1) RETURNING *', [userId, workoutId, exerciseName]);
+        if (exRes.rows.length === 0 ) return;
+        const exerciseId = exRes.rows[0].id;
+        const { rows } = await db.query(`INSERT INTO sets (exercise_id, set_number) VALUES ($1, 1) RETURNING *`, [exerciseId]);
+
+        await client.query('COMMIT');
+        return rows[0];
+	} catch (error) {
+		await client.query('ROLLBACK');
+		throw error;
+	} finally {
+		client.release();
+	}
 }
 
 export async function deleteExercise(userId: string, exerciseId: string, workoutId: string) {
