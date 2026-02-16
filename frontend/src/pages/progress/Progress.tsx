09ccd7e098ba99524952ea0e9d5a84e27e84789d
Darrel <128795218+darreleng@users.noreply.github.com>
2026-02-17 00:09:01 +0800

    Add selection box for exercise

--- frontend/src/pages/progress/Progress.tsx
+++ frontend/src/pages/progress/Progress.tsx
@@ -1,5 +1,5 @@
 import { LineChart } from '@mantine/charts';
-import { ScrollArea, Box, Paper, Title, Loader, Combobox, useCombobox, TextInput, Text, Center } from '@mantine/core';
+import { ScrollArea, Box, Paper, Title, Loader, Combobox, useCombobox, TextInput, Text, Center, Select } from '@mantine/core';
 import { useQuery } from '@tanstack/react-query';
 import { useState } from 'react';
 
@@ -33,7 +33,6 @@ function ChartTooltip({ label, payload, chartType }: ChartTooltipProps) {
             </Text>
             {payload.map((item: any) => (
                 <Text key={item.name} c={item.color} fz="sm">
-                    {/* {chartType === 'volume' ? `Total Volume: ${item.value} kg` : `Estimated 1RM: ${item.value} kg`} */}
                     {chartType === 'volume' ? `Total Volume: ` : `Estimated 1RM: `} {item.value} kg
                 </Text>
             ))}
@@ -46,7 +45,7 @@ export default function Progress(){
         onDropdownClose: () => combobox.resetSelectedOption(),
     });
     const [loading, setLoading] = useState(false);
-    const [search, setSearch] = useState('Squats');
+    const [value, setValue] = useState('');
     const { 
         data: exercises,
         isPending,
@@ -64,11 +63,10 @@ export default function Progress(){
     if (isPending) return <div><Loader size='sm' /></div>; 
     if (error) return <div>Error: {error.message}</div>;
 
-    console.log(exercises);
+    const uniqueExerciseNames = [... new Set(exercises.map(ex => ex.name))];
+    const selectedExercise = exercises.filter(ex => ex.name.toLowerCase() === value.toLowerCase()); 
 
-    const filteredExercises = exercises.filter(ex => ex.name.toLowerCase() === search.toLowerCase());
-
-    const exOneRepMax = filteredExercises.map(ex => {
+    const exOneRepMax = selectedExercise.map(ex => {
         const intensities = ex.sets
             .filter(set => set.reps > 0 && set.reps <= 12) 
             .map(set => set.weight / (1.0278 - (0.0278 * set.reps)));
@@ -82,8 +80,7 @@ export default function Progress(){
         };
     });
 
-    // TODO: Include other details like number of sets or the whole exercise details
-    const exTotalVolume = filteredExercises.map(ex => {
+    const exTotalVolume = selectedExercise.map(ex => {
         const volume = ex.sets.reduce((acc, cur) => cur.weight * cur.reps + acc, 0);
 
         return {
@@ -93,18 +90,30 @@ export default function Progress(){
         }
     })
 
+    const lowestOneRepMax = exOneRepMax.reduce((min, cur) => Math.min(min, cur.oneRepMax), exOneRepMax[0]?.oneRepMax || 0);
+    const highestOneRepMax = exOneRepMax.reduce((max, cur) => Math.max(max, cur.oneRepMax), exOneRepMax[0]?.oneRepMax || 0);
+    const lowestVolume = exTotalVolume.reduce((min, cur) => Math.min(min, cur.totalVolume), exTotalVolume[0]?.totalVolume || 0);
+    const highestVolume = exTotalVolume.reduce((max, cur) => Math.max(max, cur.totalVolume), exTotalVolume[0]?.totalVolume || 0);
+   
+
     return (    
         
         <Paper withBorder p="md" radius="md" w={1000}>
+                <Select
+                    label="Select an exercise"
+                    data={uniqueExerciseNames}
+                    onChange={(value) => setValue(value || '')}
+                    searchable
+                />
             <Center>
-                <Title order={1}>{search}</Title>
+                {/* TODO: fix layout shift when empty*/}
+                <Title order={1}>{value}</Title> 
             </Center>
 
             <Title order={4} mb="lg">Total Volume Over Time</Title> 
         
             <ScrollArea offsetScrollbars="x">
                 
-                {/* Adjust box size */}
                 <Box>
                 <LineChart
                     h={300}
@@ -114,18 +123,11 @@ export default function Progress(){
                         { name: 'totalVolume', color: 'blue' },
                     ]}
                     curveType="monotone"
-                    tickLine="x"
                     unit=' kg'
                     gridAxis="xy"
                     withXAxis={false}
-                    // xAxisProps={{ tick: false }}
-                    xAxisProps={{
-                        tickFormatter: (value) => 
-                            new Date(value).toLocaleDateString('en-GB', { 
-                                month: 'short', 
-                                day: 'numeric' 
-                            }),
-                    }}
+                    yAxisProps={{ domain: [Math.round(lowestVolume * .9), Math.round(highestVolume * 1.1)]}}
+                    xAxisProps={{ interval: 0 }}
                     lineChartProps={{ syncId: 'chart' }}
                     tooltipProps={{
                         content: ({ label, payload }) => <ChartTooltip label={label} payload={payload} chartType='volume'/>,
@@ -136,14 +138,14 @@ export default function Progress(){
 
             <Title order={4} mb="lg">Estimated 1RM Over Time</Title> 
         
-            <ScrollArea offsetScrollbars="x">
+            <ScrollArea w={'clamp(90vw, 600, 1200)'} offsetScrollbars="x">
                 
-                {/* Adjust box size */}
-                <Box>
+                {/* <Box w={400} style={{overflowX: 'auto'}}> */}
                 <LineChart
                     h={300}
                     data={exOneRepMax}
                     dataKey="date"
+                    fillOpacity={1}
                     series={[
                         { name: 'oneRepMax', color: 'grape' },
                     ]}
@@ -151,6 +153,7 @@ export default function Progress(){
                     tickLine="x"
                     unit=' kg'
                     gridAxis="xy"
+                    yAxisProps={{ domain: [Math.round(lowestOneRepMax * .9), Math.round(highestOneRepMax * 1.1)]}}
                     xAxisProps={{
                         tickFormatter: (value) => 
                             new Date(value).toLocaleDateString('en-GB', { 
@@ -163,8 +166,9 @@ export default function Progress(){
                         content: ({ label, payload }) => <ChartTooltip label={label} payload={payload} chartType='weight' />,
                     }}
                 />
-                </Box>
+                {/* </Box> */}
             </ScrollArea>
+            <Text fz={'sm'}>*Estimated 1RM is calculated using your top set (the set with the most volume under 13 sets) and the Brzycki formula: weight / ( 1.0278 – 0.0278 × reps ).</Text>
         </Paper>
         
     );
