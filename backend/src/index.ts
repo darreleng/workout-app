e9eead38ba1e5f82ff521ee5dba77841804bfd74
Darrel <128795218+darreleng@users.noreply.github.com>
2026-02-27 15:27:24 +0800

    Fix bug

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -25,7 +25,7 @@ app.use('/api/exercises', globalExerciseRoutes);
 if (process.env.NODE_ENV === 'production') {
     const root = path.join(__dirname, '../../../../frontend/dist');
     app.use(express.static(root));
-    app.get('/:any*', (req, res) => {
+    app.get('/{*splat}', (req, res) => {
         res.sendFile(path.join(root, 'index.html'));
     });
 }
