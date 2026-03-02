d397f8a1118c7d15f32f9a68e69321ba7da660a8
Darrel <128795218+darreleng@users.noreply.github.com>
2026-03-02 18:12:03 +0800

    Fix cold server bug

--- frontend/src/auth-client.ts
+++ frontend/src/auth-client.ts
@@ -2,4 +2,8 @@ import { createAuthClient } from "better-auth/react"
 
 export const authClient = createAuthClient({
     baseURL: import.meta.env.BETTER_AUTH_URL,
+    retry: {
+        attempts: 3,
+        delay: 1500
+    }
 })
